a548c939a5cafa87506392ac50ac77face3cd4f6
WeaveFeather <moonbite233@gmail.com>
2025-12-31 14:54:27 +0800

    add more

--- main.cpp
+++ main.cpp
@@ -8,8 +8,10 @@ using namespace mp_units::si::unit_symbols;
 
 int main()
 {
-    quantity<si::metre, float> length = 3 * m;
-    quantity<angular::degree, float> de = 3 * angular::degree;
-    std::println("{}, {}", length, de);
+    quantity<si::metre> length = 3 * m;
+    quantity<angular::degree> de = 3 * angular::degree;
+    quantity<mA> current = 3 * mA;
+    quantity<angular::revolution> round = 3 * angular::revolution;
+    std::println("{}, {}, {}, {}", length, de, current, round);
     return 0;
 }
